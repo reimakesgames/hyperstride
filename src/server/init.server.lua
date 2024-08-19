882afb3bb2863add5cdd110e8998057b312df3f1
rei <reimakesgames.dev@gmail.com>
2024-08-19 22:00:20 +0800

    add weapon stuff

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,13 @@
-print("Hello world, from server!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local RemoteEvents = ReplicatedStorage:WaitForChild("Events")
+
+local FireRemoteEvent: RemoteEvent = RemoteEvents:WaitForChild("Fire")
+
+FireRemoteEvent.OnServerEvent:Connect(function(player: Player, startPosition: Vector3, endPosition: Vector3, speed: number)
+	for _, otherPlayer in ipairs(game.Players:GetPlayers()) do
+		if (otherPlayer ~= player) then
+			FireRemoteEvent:FireClient(otherPlayer, startPosition, endPosition, speed)
+		end
+	end
+end)
